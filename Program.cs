2e8aa8010764884eb1a80fb08c5a24ac034e051b
Matias-Galvan <matiasgalvan90@gmail.com>
2022-05-11 20:25:58 -0300

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TPF_Comision_1_Matias_Galvan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
