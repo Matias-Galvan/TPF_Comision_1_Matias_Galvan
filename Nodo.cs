598efb0e54453245fd985a085efe4d89d49daf7f
Matias-Galvan <matiasgalvan90@gmail.com>
2022-06-12 21:37:35 -0300

    Cambios en clases generales

---
+++ Nodo.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+namespace TPF_Comision_1_Matias_Galvan
+{
+    public class Nodo<T>
+    {
+        private T dato;
+        private List<Nodo<T>> hijos;
+
+        public Nodo(T dato){
+            this.dato = dato;
+            this.hijos = new List<Nodo<T>>();
+        }
+
+        public T getDato(){
+            return this.dato;
+        }
+
+        public List<Nodo<T>> getHijos(){
+            return this.hijos;
+        }
+
+        public void setDato(T dato){
+            this.dato = dato;
+        }
+
+        public void setHijos(List<Nodo<T>> hijos){
+            this.hijos = hijos;
+        }
+    }
+}
